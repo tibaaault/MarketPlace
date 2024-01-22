699e9a397d8181073e3361a3b79eec674b9d4b15
tibaaault <113906225+tibaaault@users.noreply.github.com>
2024-01-22 16:17:30 +0100

    up

--- BDD/marketPlace-2.sql
+++ BDD/marketPlace-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mer. 27 déc. 2023 à 18:39
+-- Généré le : lun. 22 jan. 2024 à 15:11
 -- Version du serveur : 5.7.39
 -- Version de PHP : 8.2.0
 
@@ -46,7 +46,7 @@ CREATE TABLE `product` (
   `name` varchar(50) NOT NULL,
   `price` double NOT NULL,
   `type` varchar(50) NOT NULL,
-  `img_url` varchar(50) NOT NULL,
+  `img_url` text NOT NULL,
   `description` text NOT NULL,
   `id_seller` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -56,17 +56,19 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id_product`, `name`, `price`, `type`, `img_url`, `description`, `id_seller`) VALUES
-(1, 'Xiaomi TV LED 4K 108 cm Mi P1E 43 Pouces', 289, 'Digital', 'Xiaomi', 'Taille de l\'écran : 43 Pouces\r\nMarque : Xiaomi\r\nServices Internet pris en charge :	Netflix, Amazon Instant Video, YouTube\r\nTechnologie d\'affichage	: LED\r\nDimensions du produit : 8,5P x 38,1l x 24, 1H centimètres\r\nRésolution : 4K\r\nFréquence de rafraîchissement : 60 Hz\r\nCaractéristique spéciale : contrôle parental, teletext, channel edit, hbbtv, sous-titres\r\nNom de modèle : Xiaomi UHD Android TV P1E 43\"\r\nTechnologie de connectivité', 1),
-(2, 'Samsung Galaxy Tab A9+', 239, 'Digital', 'samsung-galaxy-tab-a9+', 'Marque	SAMSUNG\r\nNom de modèle	Galaxy Tab A9+\r\nTaille de l\'écran 11 Pouces\r\nRésolution d\'affichage maximale	1920 x 1200 Pixels\r\nSystème d\'exploitation Android 13, Samsung One UI 5.1.1\r\nCouleur	Argent\r\nTaille de la mémoire RAM installée 4 Go\r\nAnnée du modèle	2023\r\nPoids de l\'article 480 Grammes', 1),
-(5, 'Google Pixel 6 Pro', 899.99, 'Digital', 'google-pixel-6-pro', 'Marque	GOOGLE\r\nModèle Pixel 6 Pro\r\nTaille de l\'écran 6.7 Pouces\r\nRésolution d\'affichage maximale 1440 x 3120 Pixels\r\nSystème d\'exploitation Android 12\r\nCouleur	Noir\r\nMémoire RAM installée 8 Go\r\nAnnée du modèle	2023\r\nPoids de l\'article 210 Grammes', 1),
-(6, 'Dell XPS 13', 1299, 'Digital', 'dell-xps-13', 'Marque	DELL\r\nModèle XPS 13\r\nTaille de l\'écran 13.4 Pouces\r\nRésolution d\'affichage maximale 3840 x 2400 Pixels\r\nProcesseur Intel Core i7\r\nMémoire RAM installée 16 Go\r\nStockage SSD 512 Go\r\nAnnée du modèle	2023\r\nPoids de l\'article 1.2 Kilogrammes', 1),
-(7, 'Sony Alpha a7 IV', 1999.99, 'Digital', 'sony-alpha-a7-iv', 'Marque	SONY\r\nModèle Alpha a7 IV\r\nType de capteur CMOS\r\nRésolution du capteur 33 Megapixels\r\nEnregistrement vidéo 4K UHD\r\nStabilisation d\'image intégrée\r\nAnnée du modèle	2023\r\nPoids de l\'article 657 Grammes', 1),
-(8, 'Apple AirPods Pro', 249.99, 'Digital', 'apple-airpods-pro', 'Marque	APPLE\r\nModèle AirPods Pro\r\nType d\'écouteurs Intra-auriculaire\r\nAutonomie de la batterie jusqu\'à 4.5 heures\r\nRéduction du bruit active\r\nÉtanche IPX4\r\nAnnée du modèle	2023', 1),
-(9, 'ASUS ROG Swift PG279Q', 699, 'Digital', 'asus-rog-swift-pg279q', 'Marque	ASUS\r\nModèle ROG Swift PG279Q\r\nTaille de l\'écran 27 Pouces\r\nRésolution d\'affichage maximale 2560 x 1440 Pixels\r\nTaux de rafraîchissement 165 Hz\r\nTechnologie G-Sync\r\nAnnée du modèle	2023\r\nPoids de l\'article 7 Kilogrammes', 1),
-(10, 'Nintendo Switch OLED', 349.99, 'Digital', 'nintendo-switch-oled', 'Marque	NINTENDO\r\nModèle Switch OLED\r\nÉcran OLED 7 Pouces\r\nStockage interne 64 Go\r\nMode téléviseur, portable et sur table\r\nJoy-Cons amovibles\r\nAnnée du modèle	2023\r\nPoids de l\'article 320 Grammes', 1),
-(11, 'Creality Ender 3 V2', 299, 'Digital', 'creality-ender-3-v2', 'Marque	CREALITY\r\nModèle Ender 3 V2\r\nVolume d\'impression 220 x 220 x 250 mm\r\nÉcran tactile LCD\r\nPlateau chauffant\r\nAssemblage facile\r\nAnnée du modèle	2023\r\nPoids de l\'article 7 Kilogrammes', 1),
-(12, 'Netgear Orbi RBK753', 449.99, 'Digital', 'netgear-orbi-rbk753', 'Marque	NETGEAR\r\nModèle Orbi RBK753\r\nCouverture Wi-Fi jusqu\'à 600 m²\r\nTechnologie Tri-Band\r\nConnexion jusqu\'à 40 appareils\r\nContrôle parental\r\nAnnée du modèle	2023\r\nPoids de l\'article 1.2 Kilogrammes', 1),
-(13, 'Kindle Paperwhite', 129.99, 'Digital', 'kindle-paperwhite', 'Marque	AMAZON\r\nModèle Paperwhite\r\nÉcran tactile 6 Pouces\r\nRésistant à l\'eau IPX8\r\nÉclairage intégré\r\nAutonomie de la batterie jusqu\'à 10 semaines\r\nAnnée du modèle	2023\r\nPoids de l\'article 182 Grammes', 1);
+(1, 'Xiaomi TV LED 4K 108 cm Mi P1E 43 Pouces', 289, 'Digital', 'Xiaomi.png', 'Taille de l\'écran : 43 Pouces\r\nMarque : Xiaomi\r\nServices Internet pris en charge :	Netflix, Amazon Instant Video, YouTube\r\nTechnologie d\'affichage	: LED\r\nDimensions du produit : 8,5P x 38,1l x 24, 1H centimètres\r\nRésolution : 4K\r\nFréquence de rafraîchissement : 60 Hz\r\nCaractéristique spéciale : contrôle parental, teletext, channel edit, hbbtv, sous-titres\r\nNom de modèle : Xiaomi UHD Android TV P1E 43\"\r\nTechnologie de connectivité', 1),
+(2, 'Samsung Galaxy Tab A9+', 239, 'Digital', 'samsung-galaxy-tab-a9+.png', 'Marque	SAMSUNG\r\nNom de modèle	Galaxy Tab A9+\r\nTaille de l\'écran 11 Pouces\r\nRésolution d\'affichage maximale	1920 x 1200 Pixels\r\nSystème d\'exploitation Android 13, Samsung One UI 5.1.1\r\nCouleur	Argent\r\nTaille de la mémoire RAM installée 4 Go\r\nAnnée du modèle	2023\r\nPoids de l\'article 480 Grammes', 1),
+(5, 'Google Pixel 6 Pro', 950, 'Digital', 'google-pixel-6-pro.png', 'Marque	GOOGLE\r\nModèle Pixel 6 Pro\r\nTaille de l\'écran 6.7 Pouces\r\nRésolution d\'affichage maximale 1440 x 3120 Pixels\r\nSystème d\'exploitation Android 12\r\nCouleur	Noir\r\nMémoire RAM installée 8 Go\r\nAnnée du modèle	2023\r\nPoids de l\'article 210 Grammes', 1),
+(6, 'Dell XPS 13', 1299, 'Digital', 'dell-xps-13.png', 'Marque	DELL\r\nModèle XPS 13\r\nTaille de l\'écran 13.4 Pouces\r\nRésolution d\'affichage maximale 3840 x 2400 Pixels\r\nProcesseur Intel Core i7\r\nMémoire RAM installée 16 Go\r\nStockage SSD 512 Go\r\nAnnée du modèle	2023\r\nPoids de l\'article 1.2 Kilogrammes', 1),
+(7, 'Sony Alpha a7 IV', 1999.99, 'Digital', 'sony-alpha-a7-iv.png', 'Marque	SONY\r\nModèle Alpha a7 IV\r\nType de capteur CMOS\r\nRésolution du capteur 33 Megapixels\r\nEnregistrement vidéo 4K UHD\r\nStabilisation d\'image intégrée\r\nAnnée du modèle	2023\r\nPoids de l\'article 657 Grammes', 1),
+(8, 'Apple AirPods Pro', 249.99, 'Digital', 'apple-airpods-pro.png', 'Marque	APPLE\r\nModèle AirPods Pro\r\nType d\'écouteurs Intra-auriculaire\r\nAutonomie de la batterie jusqu\'à 4.5 heures\r\nRéduction du bruit active\r\nÉtanche IPX4\r\nAnnée du modèle	2023', 1),
+(9, 'ASUS ROG Swift PG279Q', 699, 'Digital', 'asus-rog-swift-pg279q.png', 'Marque	ASUS\r\nModèle ROG Swift PG279Q\r\nTaille de l\'écran 27 Pouces\r\nRésolution d\'affichage maximale 2560 x 1440 Pixels\r\nTaux de rafraîchissement 165 Hz\r\nTechnologie G-Sync\r\nAnnée du modèle	2023\r\nPoids de l\'article 7 Kilogrammes', 1),
+(10, 'Nintendo Switch OLED', 349.99, 'Digital', 'nintendo-switch-oled.png', 'Marque	NINTENDO\r\nModèle Switch OLED\r\nÉcran OLED 7 Pouces\r\nStockage interne 64 Go\r\nMode téléviseur, portable et sur table\r\nJoy-Cons amovibles\r\nAnnée du modèle	2023\r\nPoids de l\'article 320 Grammes', 1),
+(11, 'Creality Ender 3 V2', 299, 'Digital', 'creality-ender-3-v2.png', 'Marque	CREALITY\r\nModèle Ender 3 V2\r\nVolume d\'impression 220 x 220 x 250 mm\r\nÉcran tactile LCD\r\nPlateau chauffant\r\nAssemblage facile\r\nAnnée du modèle	2023\r\nPoids de l\'article 7 Kilogrammes', 1),
+(12, 'Netgear Orbi RBK753', 449.99, 'Digital', 'netgear-orbi-rbk753.png', 'Marque	NETGEAR\r\nModèle Orbi RBK753\r\nCouverture Wi-Fi jusqu\'à 600 m²\r\nTechnologie Tri-Band\r\nConnexion jusqu\'à 40 appareils\r\nContrôle parental\r\nAnnée du modèle	2023\r\nPoids de l\'article 1.2 Kilogrammes', 1),
+(13, 'Kindle Paperwhite', 129.99, 'Digital', 'kindle-paperwhite.png', 'Marque	AMAZON\r\nModèle Paperwhite\r\nÉcran tactile 6 Pouces\r\nRésistant à l\'eau IPX8\r\nÉclairage intégré\r\nAutonomie de la batterie jusqu\'à 10 semaines\r\nAnnée du modèle	2023\r\nPoids de l\'article 182 Grammes', 1),
+(22, 'Product 03', 40, 'Digital', '165ae6ed51a403.webp', 'Ajout bdd ', 1),
+(23, 'Image mignon', 45, 'Immobilier', '1659ea6e17c807.webp', 'Je vends ma super image des mignons \r\nTrès bon état', 1);
 
 -- --------------------------------------------------------
 
@@ -109,6 +111,13 @@ CREATE TABLE `review` (
   `id_product` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `review`
+--
+
+INSERT INTO `review` (`id_review`, `rating`, `comment`, `date_review`, `id_user`, `id_product`) VALUES
+(1, 5, NULL, '2024-01-22', 2, 8);
+
 -- --------------------------------------------------------
 
 --
@@ -129,7 +138,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `name`, `firstname`, `role`) VALUES
-(1, 'troelstrate2@myges.fr', 'tibo', 'Thibault', 'Roelstrate', 1);
+(1, 'troelstrate2@myges.fr', 'tibo', 'Thibault', 'Roelstrate', 1),
+(2, 'customer@gmail.com', 'test', 'Customer', 'ManyThings', 0);
 
 --
 -- Index pour les tables déchargées
@@ -192,7 +202,7 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT pour la table `purchase`
@@ -204,13 +214,13 @@ ALTER TABLE `purchase`
 -- AUTO_INCREMENT pour la table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
