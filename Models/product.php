a4bf5d8d437bb0dd54f95a2e1d9bb1f2e0f11d94
tibaaault <113906225+tibaaault@users.noreply.github.com>
2024-01-22 16:18:29 +0100

    up

--- Models/product.php
+++ Models/product.php
@@ -16,7 +16,7 @@ function AllProducts()
     $db = Connexion();
     
     try {
-        $statement = $db->prepare("SELECT * FROM product, review WHERE product.id_product = review.id_product");
+        $statement = $db->prepare("SELECT * FROM product");
         $statement->execute();
         $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     } catch (Exception $e) {
